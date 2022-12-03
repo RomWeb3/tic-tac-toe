b0bc0e5baab0727c7bc8a36c2eb83e1ac2964ae1
RomWeb3 <mspulco07@gmail.com>
2022-12-03 14:41:04 +0100

    game vs cpu easy mode done

--- starter-code/app.js
+++ starter-code/app.js
@@ -27,17 +27,22 @@ const playVsPlayer = document.querySelector('.player');
 const menu = document.querySelector('.new-game-menu');
 const game = document.querySelector('.game');
 let currentPlayer;
+let xOrO;
+let oOrX;
 
 playVsCpu.addEventListener('click', () => {
     menu.classList.remove('active');
-    game.classList.add('active');
-    game.classList.add('cpu');
+    game.classList.add('active', 'cpu');
     if (pickX.classList.contains('active')) {
         game.classList.add('pickX')
         currentPlayer = 'X';
+        xOrO = 'x';
+        oOrX = 'o';
     } else {
         game.classList.add('pickO')
         currentPlayer = 'O';
+        xOrO = 'o';
+        oOrX = 'x';
         setTimeout(cpuFirstMove, 500);
     }
 });
@@ -61,6 +66,8 @@ const cell8 = document.querySelector('#cell-8');
 const cell9 = document.querySelector('#cell-9');
 const cells = document.querySelectorAll('.cell');
 let moveCounter = 0;
+const modal = document.querySelector('.modal');
+const winOrLoseTxt = document.querySelector('.win-or-lose-txt');
 const drawModal = document.querySelector('.modal-draw');
 let gameActive = true;
 
@@ -84,16 +91,16 @@ function player() {
                 }
                 setTimeout(cpu, 500);
                 moveCounter++;
+                result('player');
         })          
-        }
-    
-)}
+        })
+}
 
 function cpu() {
     const random = Math.floor(Math.random() * 9) + 1;
     const cell = document.querySelector(`#cell-${random}`);
     
-    if (moveCounter < 9) {
+    if (moveCounter < 9 && gameActive === true) {
         if (cell.childElementCount !== 0) {
             return cpu();
         } else if (cell.childElementCount === 0 && game.classList.contains('pickX')) {
@@ -102,12 +109,14 @@ function cpu() {
             cell.append(img);
             moveCounter++;
             switchTurn('X');
+            result('cpu')
         } else if (cell.childElementCount === 0 && game.classList.contains('pickO')) {
             const img = document.createElement('img');
             img.src = "./assets/icon-x.svg";
             cell.append(img);
             moveCounter++;
             switchTurn('O');
+            result('cpu')
         } 
     }
 }
@@ -125,6 +134,74 @@ function cpuFirstMove() {
     }
 }
 
+const winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+function result(winner) {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = cells[winCondition[0]];
+        // console.log(`a: ${a}`);
+        const b = cells[winCondition[1]];
+        // console.log(`b: ${b}`);
+        const c = cells[winCondition[2]];
+        // console.log(`c: ${c}`);
+        const aChild = a.firstElementChild;
+        // console.log(aChild);
+        const bChild = b.firstElementChild;
+        // console.log(bChild);
+        const cChild = c.firstElementChild;
+        // console.log(cChild);
+        // console.log(xOrO);
+
+        if (aChild !== null && bChild !== null && cChild !== null) {
+            if (aChild.src === bChild.src && bChild.src === cChild.src) {
+                roundWon = true;
+                break
+            }
+        }
+    }
+
+    if (roundWon && winner === 'player') {
+        gameActive = false;
+        setTimeout(() => {
+            updateScoreboard('player');
+            winOrLoseTxt.innerText = 'you won!';
+            modal.classList.add('active', xOrO);
+        }, 400);
+    } else if (roundWon && winner === 'cpu') {
+        gameActive = false;
+        setTimeout(() => {
+            updateScoreboard('cpu');
+            winOrLoseTxt.innerText = 'you lose!';
+            modal.classList.add('active', oOrX);
+        }, 400);
+        
+    } else if (moveCounter === 9) {
+        gameActive = false;
+        setTimeout(() => {
+            updateScoreboard('draw');
+            drawModal.classList.add('active');
+        }, 400);
+    }
+}
+
+// Prendre le Data-attribute des cellules winner pour chnager la couleur de la ligne gagnante
+
+
+const playerScore = document.getElementById('your-score');
+const cpuScore = document.getElementById('cpu-score');
+const tiesScore = document.getElementById('ties-score');
+
+function updateScoreboard(winner) {
+    if (winner === 'player') {
+        playerScore.innerText = parseInt(playerScore.innerText) + 1;
+    } else if (winner === 'cpu') {
+        cpuScore.innerText = parseInt(cpuScore.innerText) + 1;
+    } else if(winner === 'draw') {
+        tiesScore.innerText = parseInt(tiesScore.innerText) + 1;
+    }
+}
 
 
 function restart() {
@@ -144,6 +221,7 @@ function restart() {
             cell.innerHTML = '';
             moveCounter = 0;
             switchTurn('X');
+            gameActive = true;
         })
         restartModal.classList.remove('active');
         if (game.classList.contains('pickO')) {
@@ -160,6 +238,8 @@ function nextRound() {
             cells.forEach(cell => {
                 cell.innerHTML = '';
                 moveCounter = 0;
+                modal.classList.remove('active', 'x', 'o');
+                gameActive = true;
             })
             drawModal.classList.remove('active');
             if (game.classList.contains('pickO')) {
@@ -176,13 +256,12 @@ function quit() {
         btn.addEventListener('click', () => {
             drawModal.classList.remove('active');
             menu.classList.add('active');
-            game.classList.remove('active');
-            game.classList.remove('cpu');
-            game.classList.remove('pickX');
-            game.classList.remove('pickO');
+            game.classList.remove('active', 'cpu', 'pickX', 'pickO');
+            modal.classList.remove('active', 'x', 'o');
             cells.forEach(cell => {
                 cell.innerHTML = '';
                 moveCounter = 0;
+                gameActive = true;
             })
         })
     })
@@ -204,6 +283,7 @@ function switchTurn(currentPlayer) {
 
 function newGame() {
     player();
+    result();
     restart();
     nextRound();
     quit();
